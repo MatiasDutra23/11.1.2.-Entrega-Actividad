bbe7d4f95d4d0f0cb628bf7621a30a27380c4007
Matias Dutra <114629823+MatiasDutra23@users.noreply.github.com>
2023-09-04 09:46:00 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const apiKey = 'AQUI VA LA APIKEY';
+    const lat = -33.68272565799198; // LATITUD de mi localidad extraída de Google Maps
+    const lon = -53.55610306188749; // LONGITUD de mi localidad extraída de Google Maps
+  
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+  
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const cityName = data.name;
+        const temperatureKelvin = data.main.temp;
+        const weatherDescription = data.weather[0].description;
+  
+        // Conversión de Kelvin a Celsius
+        const temperatureCelsius = temperatureKelvin - 273.15;
+  
+        // Selecciona el elemento weather-card
+        const weatherCard = document.getElementById('weather-card');
+  
+        // Muestra los datos del clima
+        weatherCard.innerHTML = `
+            <h2>Clima en ${cityName}</h2>
+            <p>Descripción: ${weatherDescription}</p>
+            <p>Temperatura: ${temperatureCelsius.toFixed(2)}°C</p>
+        `;
+      });
+  });
+  
