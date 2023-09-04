573d600c4f3f384426a85b872917636858a61619
Matias Dutra <114629823+MatiasDutra23@users.noreply.github.com>
2023-09-04 11:18:07 -0300

    Mejora en la tarjeta y agregue iconos :D

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
+// Espera a que la página HTML se cargue completamente
 document.addEventListener("DOMContentLoaded", function () {
-    const apiKey = '2a3d2f97b6ae7ee41823afe5ff7b33d9';
-    const lat = -33.68272565799198; // LATITUD de mi localidad extraída de Google Maps
-    const lon = -53.55610306188749; // LONGITUD de mi localidad extraída de Google Maps
+  //clave de la API
+  const apiKey = '2a3d2f97b6ae7ee41823afe5ff7b33d9';
+
+  // Coordenadas de latitud y longitud de tu localidad (Puedes cambiar estas coordenadas para ver otra ubicación)
+  const latitude = -33.68272565799198; 
+  const longitude = -53.55610306188749;
+
+  // URL de la API del clima con las coordenadas y la clave de API
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
   
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-  
-    fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => {
-        const cityName = data.name;
-        const temperatureKelvin = data.main.temp;
-        const weatherDescription = data.weather[0].description;
-  
-        // Conversión de Kelvin a Celsius
-        const temperatureCelsius = temperatureKelvin - 273.15;
-  
-        // Selecciona el elemento weather-card
-        const weatherCard = document.getElementById('weather-card');
-  
-        // Muestra los datos del clima
-        weatherCard.innerHTML = `
-            <h2>Clima en ${cityName}</h2>
-            <p>Descripción: ${weatherDescription}</p>
-            <p>Temperatura: ${temperatureCelsius.toFixed(2)}°C</p>
-        `;
-      });
-  });
-  
+  fetch(apiUrl)
+    .then(response => response.json()) // Convierte la respuesta en formato JSON
+    .then(data => {
+      const cityName = data.name; // Nombre de la ciudad
+      const temperatureKelvin = data.main.temp; // Temperatura en Kelvin
+      const weatherDescription = data.weather[0].description; // Descripción del clima
+
+      // Convierte la temperatura de Kelvin a Celsius
+      const temperatureCelsius = temperatureKelvin - 273.15;
+
+      // Selecciona los elementos del HTML donde mostraremos los datos del clima
+      const cityElement = document.getElementById('cityName');
+      const weatherDescriptionElement = document.getElementById('weatherDescription');
+      const temperatureElement = document.getElementById('temperature');
+      const weatherIconElement = document.querySelector('.weather-icon');
+
+      // Muestra los datos del clima en la página web
+      cityElement.textContent = cityName;
+      weatherDescriptionElement.textContent = weatherDescription;
+      temperatureElement.textContent = temperatureCelsius.toFixed(2);
+
+      // Asigna una clase de icono de clima según la descripción del clima
+if (weatherDescription.toLowerCase().includes('clear')) {
+  weatherIconElement.classList.add('sun'); // Icono de sol
+} else if (weatherDescription.toLowerCase().includes('cloud')) {
+  weatherIconElement.classList.add('cloud'); // Icono de nube
+} else if (weatherDescription.toLowerCase().includes('rain')) {
+  weatherIconElement.classList.add('light-rain'); // Icono de lluvia ligera
+} else {
+  weatherIconElement.classList.add('default-icon'); // Icono predeterminado
+}
+
+
+    });
+});
